512bd1cc1a04050412e459f6dfba71e2d3d87240
Dmytro Samchuk <dsamchuk_ext@germanpersonnel.de>
2018-03-20 09:59:20 +0200

    setup added

--- char_split.py
+++ char_split.py
@@ -5,9 +5,10 @@ Split German compound words
 autor: don.tuggener@gmail.com
 """
 
-import ngram_probs  # trained with char_split_train.py
+import charsplit.pretrained.ngram_probs  # trained with char_split_train.py
 import re
 import sys
+
 
 def split_compound(word):
     """ Return list of possible splits, best first """
@@ -44,11 +45,11 @@ def split_compound(word):
             # Probability of first compound, given by its ending prob
             if pre_slice_prob == [] and k <= len(pre_slice):
                 end_ngram = pre_slice[-k:]  # Look backwards
-                pre_slice_prob.append(ngram_probs.suffix.get(end_ngram, -1))    # Punish unlikely pre_slice end_ngram
+                pre_slice_prob.append(pretrained.ngram_probs.suffix.get(end_ngram, -1))    # Punish unlikely pre_slice end_ngram
                     
             # Probability of ngram in word, if high, split unlikely
             in_ngram = word[n:n+k]
-            in_slice_prob.append(ngram_probs.infix.get(in_ngram, 1)) # Favor ngrams not occuring within words
+            in_slice_prob.append(pretrained.ngram_probs.infix.get(in_ngram, 1)) # Favor ngrams not occuring within words
                                   
             # Probability of word starting
             if start_slice_prob == []:
@@ -57,7 +58,7 @@ def split_compound(word):
                 if ngram.endswith('ts') or ngram.endswith('gs') or ngram.endswith('ks') or ngram.endswith('hls') or ngram.endswith('ns'):
                     if len(ngram[:-1]) > 2:
                         ngram = ngram[:-1] 
-                start_slice_prob.append(ngram_probs.prefix.get(ngram, -1))
+                start_slice_prob.append(pretrained.ngram_probs.prefix.get(ngram, -1))
 
         if pre_slice_prob == [] or start_slice_prob == []: continue
         
@@ -70,23 +71,6 @@ def split_compound(word):
     scores.sort(reverse=True)
     if scores == []: scores=[ [0., word.title(), word.title()] ]
     return sorted(scores, reverse = True)
-
-
-def test():
-    """ Test on Germanet compounds from http://www.sfs.uni-tuebingen.de/lsd/compounds.shtml """
-    cases, correct = 0., 0.
-    for line in open('split_compounds_from_GermaNet9.0.txt','r'):    # Cases with hypens removed!
-        cases += 1
-        sys.stderr.write('\r'+str(cases))
-        sys.stderr.flush()
-        line_orig = str(line)
-        line = line.strip().split('\t')
-        if not len(line)==3: continue   # A few corrupted lines
-        head = split_compound(line[0])
-        if head != [] and head[0][2].encode('utf8') == line[2]: correct += 1
-        #else: print line, head; pdb.set_trace()    # See errors
-        if cases % 10000 == 0: print(' Accuracy (' + str(correct) + '/' + str(cases) + '): ' + 100*correct/cases)
-    print(' Accuracy (' + str(correct) + '/' + str(cases) + '): ' + 100*correct/cases)
 
 
 if __name__ == '__main__':
---
+++ setup.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2018-2022 Dmytro Samchuk
+
+DESCRIPTION = "CharSplit - An *ngram*-based compound splitter for German"
+LONG_DESCRIPTION = """\
+Splits a German compound into its body and head, e.g.
+> Autobahnraststätte -> Autobahn - Raststätte
+Implementation of the method decribed in the appendix of the thesis:
+Tuggener, Don (2016). *Incremental Coreference Resolution for German.* University of Zurich, Faculty of Arts.
+**TL;DR**: The method calculates probabilities of ngrams occurring at the beginning, end and in the middle of words and identifies the most likely position for a split.
+The method achieves ~95% accuracy for head detection on the [Germanet compound test set](http://www.sfs.uni-tuebingen.de/lsd/compounds.shtml).
+A model is provided, trained on 10 Mio German nouns from newspaper text.
+"""
+DISTNAME = 'charsplit'
+MAINTAINER = 'Dmytro Samchuk'
+MAINTAINER_EMAIL = 'dvsamchuk@gmail.com'
+URL = 'https://github.com/Codealist/CharSplit'
+LICENSE = 'GNU GPL-3.0'
+DOWNLOAD_URL = 'https://github.com/Codealist/CharSplit/releases'
+VERSION = 'v1.3.5.1'
+
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+
+if __name__ == "__main__":
+
+    setup(name=DISTNAME,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        install_requires=[],
+        packages=['charsplit', 'charsplit.pretrained'],
+        classifiers=[
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
+                     'License :: OSI Approved :: {}'.format(LICENSE),
+                     'Topic :: Scientific/Engineering :: Natural language processing',
+                     'Operating System :: POSIX',
+                     'Operating System :: Unix',
+                     'Operating System :: MacOS'])
